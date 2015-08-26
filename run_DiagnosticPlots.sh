2269c4ac1e496ddf50e7e1398c7bb619d7afa69e
Joseph H Kennedy <kennedyjh@ornl.gov>
2015-08-26 12:22:12 -0400

    Get plots to work with GDrive provided data

--- run_DiagnosticPlots.sh
+++ run_DiagnosticPlots.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-export filestart=170 
-export fileend=200
-export dataF="Greenland_5km_v1.1_SacksRev_c110629.nc" 
-export modelPreName="BG_CISM1_parallel_updating.cism.h."
+export filestart=208 
+export fileend=210
+export dataF="data/Greenland_5km_v1.1_SacksRev_c110629.nc" 
+export modelPreName="data/BG_CISM1_parallel_updating.cism.h."
 export modelPostName="-01-01-00000.nc"
 ncl DiagnosticPlots.ncl
